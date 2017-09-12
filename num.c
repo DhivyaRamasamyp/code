44e0ca3f2e5013c54dec29249c1dd225ef34a057
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2017-09-12 15:51:24 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number:/n");
+scanf("%d",&n);
+if(n>0)
+{
+printf("the number is positive/n");
+}[
+if(n<0)
+{
+printf("the number is negative/n");
+}
+if(n==0)
+{
+printf("the number is zero/n");
+}
+}
