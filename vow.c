6e298eea8e7f9a30ef3e98a92a5c5ecb6ea82bfc
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-01-09 16:23:40 +0530

    Update vow.c

--- vow.c
+++ vow.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-char ch[4];
+char ch[5];
 printf("enter a letter\n");
 scanf("%c",&ch);
 if(ch=='a'||ch=='A'||ch=='E'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='I'||ch=='O'||ch=='U')
