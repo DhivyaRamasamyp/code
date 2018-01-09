8c76262222cc4b653fc926ccba69e1bd536fc96c
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-01-09 16:12:40 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a=3,b=4,c=5;
+if(a>b&&a>c)
+{
+printf("\n %d is large",a);
+}
+elseif(b>a&&b>c)
+{
+printf("\n %d is lage",b);
+}
+else
+{
+printf("\n %d is large",c);
+}
+}
