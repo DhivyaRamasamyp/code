cd3e10024bc327d87e76724b077717d8e74d1899
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-01 08:50:01 +0000

    Update pri.c

--- pri.c
+++ pri.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-int a,i,count==1;
+int pri,i,count==1;
 printf("\n enter the nunmber");
-scanf("%d",&a);
-for(i=2;i<a;i++)
+scanf("%d",&pri);
+for(i=2;i<pri;i++)
 {
-if(a%i==0)
+if(pri%i==0)
 count++;
 }
 if(count==1)
