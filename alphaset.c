b2f72e0270e707c51c974d5877cf9f499fb2c7cd
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-01 07:50:33 +0000

    Create alphaset.c

---
+++ alphaset.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char c;
+clrscr();
+printf("enter the letter\n");
+scanf("%c",&c);
+if(c!='1'&&c!='2'&&c!='3'&&c!='4'&&c!='5'&&c!='6'&&c!='7'&&c!='8'&&c!='9')
+{
+printf("alphabet\n")
+}
+else
+{
+printf("no alphabet");
+}
+}
