971ffa4db16ac741b7e118c2b04fba1f402181ac
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-01 08:14:00 +0000

    Create kknum.c

---
+++ kknum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i,a[100],sum=0;
+printf("\n enter num");
+scanf("%d%d",&n,&k);
+printf("\n enter array");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+sum=sum+a[i]);
+}
+printf("%d",sum);
+}
