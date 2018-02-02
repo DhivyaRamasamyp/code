4e3e71ee60f8a99f570e507dbb60d1218f2e5f72
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-02 14:10:54 +0530

    Create pholi.c

---
+++ pholi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+printf("\n enter a day");
+scanf("%s",a);
+if(a=="saturday"||a=="sunday")
+{
+printf("\n holiday yes");
+}
+else
+{ printf("\n no holiday");
+}
+}
