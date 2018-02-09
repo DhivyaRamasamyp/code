52943a73c27fcbd3fd099350aaf386d9059a558f
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-09 14:39:55 +0530

    Create vowelc.c

---
+++ vowelc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+char a[100],i;
+printf("\n enter the string");
+scanf("%s",a);
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+{
+    printf("\n vowel");
+}
+else
+{
+    printf("\n consonants");
+}
+}
