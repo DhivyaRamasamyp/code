98e294a026daf5a51dfce125589237936307f2e4
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-13 16:52:38 +0530

    Create m94.c

---
+++ m94.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i,a[100];
+    printf("\n enter the n and k num");
+    scanf("%d%d",&k,&n);
+    for(i=1;i<k;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    
+        printf("\n %d",a[n]);
+    return 0;
+}
