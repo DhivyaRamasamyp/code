97ecd75bdfb12f194f749cb79078344a17c16423
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-13 16:06:59 +0530

    Create si.c

---
+++ si.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int p,t,r,si;
+    printf("\n enter the p t r values");
+    scanf("%d\t%d\t%d\t",&p,&t,&r);
+    si=(p*t*r)/100;
+    printf("%d is simple interest",si);
+return 0;
+}
