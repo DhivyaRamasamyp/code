809928fb7b906061b6a138437a9d976d6c5114f2
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-22 16:04:32 +0530

    Create numword.c

---
+++ numword.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+ void main()
+ {
+     int n;
+     printf("\n enter the value of n");
+     scanf("%d",&n);
+     switch(n)
+     {
+         case 1:
+         printf("one");
+         break;
+         case 2:
+         printf("\n two");
+         break;
+         case 3:
+         printf("\n three");
+         break;
+         case 4:
+         printf("\n four");
+         break;
+         case 5:
+         printf("\n five");
+         break;
+         case 6:
+         printf("\n six");
+         break;
+         case 7:
+         printf("\n seven");
+         break;
+         case 8:
+         printf("\n eight");
+         break;
+         case 9:
+         printf("\n nine");
+         break;
+         case 10:
+         printf("\n ten");
+         break;
+         default: break;
+         
+     }
+ }
