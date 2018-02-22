ecd90ac2f8649510b383a985a62c30d772535d45
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-22 16:12:15 +0530

    Create sixodd.c

---
+++ sixodd.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ void main()
+ {
+     int n,m,c;
+     printf("\n enter the value of n,m");
+     scanf("%d%d",&n,&m);
+     c=(n+m);
+     if(c%2==0)
+     {
+         printf("\n even");
+     }
+     else
+     {
+         printf("odd");
+     }
+ }
