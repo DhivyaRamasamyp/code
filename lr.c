fe529a0bf5c72e3101125194bfb23804effbd948
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-22 16:29:29 +0530

    Create lr.c

---
+++ lr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,l,r;
+    printf("\n enter the num n,l,r");
+    scanf("%d%d%d",&n,&l,&r);
+    if(n>l&&n<r)
+    {
+        printf("\n yes");
+    }
+        else
+        {
+            printf("\n no");
+            
+        }
+}
