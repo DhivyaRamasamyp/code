348ad04de06d3bbcd34025b2368931d0b98b0ae2
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-02-27 16:28:33 +0530

    Create base.c

---
+++ base.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int base,hgt,area;
+    printf("\n enter the base and height");
+    scanf("%d%d",&base,&hgt);
+    area=(0.5*base*hgt);
+    printf("%d",area);
+}
