b2500c7c77e28cdeb5d4d5db5c00b89609abf7d0
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-03-02 14:26:45 +0530

    Create 77fac.c

---
+++ 77fac.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,a,i;
+    printf("\n enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(a=1;a<=n;a++)
+        {
+   if(n==(i*a))
+   {
+       printf("\n %d is the factor of %d",a,n);
+   }
+}
+}
+}
