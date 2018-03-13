79ca98be674ccfac38ca5f2ed4b5463327cd899d
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-03-13 16:21:59 +0530

    Create happy.c

---
+++ happy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a[100];
+    char s[100],x[100];
+    printf("\n enter the sting s and x");
+    scanf("%s%s",s,x);
+    printf("%s",s);
+    
+} 
