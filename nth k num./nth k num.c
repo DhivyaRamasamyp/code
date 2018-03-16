befb0d2444bd571c27513f8900cfecf3779bfd87
DhivyaRamasamyp <30829607+DhivyaRamasamyp@users.noreply.github.com>
2018-03-16 14:39:51 +0530

    Create nth k num.c

---
+++ nth k num./nth k num.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,i,a[100],count=0;
+    printf("\n rnter the n and k num");
+    scanf("%d%d",&n,&k);
+    printf("\n enter the array values of n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",a[i]);
+    }  
+    for(i=0;i<n;i++)
+    {
+    if(a[i]%2!=0)
+        {
+            count++;
+            if(count==1)
+            
+            {
+                printf("%d",a[i]);
+            }
+    }
+}
+}
